49526bce68b9919f8dd1245071764b5842803c76
TiffanyThani1 <tiffanythani22@gmail.com>
2025-11-17 15:20:17 -0500

    newlogo

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -3,7 +3,7 @@ export default function Navbar() {
     <nav className="fixed top-0 left-0 w-full bg-transparent py-4 px-8 z-50">
       <div className="flex items-center justify-start">
         <img
-          src="https://res.cloudinary.com/dppdnwp38/image/upload/v1762977396/logoo_icwdbg.png"
+          src="https://res.cloudinary.com/dppdnwp38/image/upload/v1763410784/logoo_oqg9ug.png"
           alt="SkinLumina Logo"
           className="h-20 w-56 object-contain"
         />
